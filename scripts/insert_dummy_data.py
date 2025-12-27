3d492b7446195b5f932143ffcb02559790cb28c5
itshowrohitworks <therohitshrivastava@gmail.com>
2025-12-27 18:37:01 +0530

    Added system path again insert data file.

--- scripts/insert_dummy_data.py
+++ scripts/insert_dummy_data.py
@@ -1,5 +1,10 @@
 import random
 from datetime import datetime, timedelta
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from config.db import get_connection
 
 N_STREAMERS = 20
