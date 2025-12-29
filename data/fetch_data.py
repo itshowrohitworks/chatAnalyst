097fc03c8ac382198954fae1f901a10e13b7e5df
itshowrohitworks <therohitshrivastava@gmail.com>
2025-12-29 18:16:50 +0530

    Changed Fetch ML functions from streamers to streams. (fetching more Rows for model training)

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -7,39 +7,35 @@ from psycopg2.extras import RealDictCursor
 def fetch_ml_data():
     query = """
         SELECT 
-        str.streamer_id,
-        str.followers,
-        str.niche,
-        str.country,
-        ROUND(AVG(stm.avg_viewers),2) AS avg_viewers,
-        ROUND(AVG(stm.peak_viewers),2) AS avg_peak_viewers,
-        ROUND(AVG(stm.chat_rate),2) AS avg_chat_rate,
-        ROUND(AVG(stm.like_count),2) AS avg_like_count,
-        SUM(stm.duration_minutes) AS total_minutes,
-        donation_totals.total_donations
-        FROM app.streamers str
-        JOIN app.streams stm ON str.streamer_id = stm.streamer_id
-        JOIN (
-            SELECT streamer_id, SUM(amount) as total_donations
-            FROM app.donations
-            GROUP BY streamer_id
-        ) donation_totals ON str.streamer_id = donation_totals.streamer_id
+            stm.stream_id,
+            str.niche,
+            str.country,
+            stm.avg_viewers,
+            stm.peak_viewers,
+            stm.chat_rate,
+            stm.like_count,
+            stm.duration_minutes,
+            -- Calculate total donations specifically for THIS stream
+            COALESCE(SUM(don.amount), 0) AS stream_donations
+        FROM app.streams stm
+        JOIN app.streamers str ON stm.streamer_id = str.streamer_id
+        LEFT JOIN app.donations don ON stm.stream_id = don.stream_id
         GROUP BY 
-            str.streamer_id, 
-            str.followers, 
+            stm.stream_id, 
             str.niche, 
-            donation_totals.total_donations
-        ORDER BY str.streamer_id ASC;
+            str.country, 
+            stm.avg_viewers, 
+            stm.peak_viewers, 
+            stm.chat_rate, 
+            stm.like_count, 
+            stm.duration_minutes
+        ORDER BY stm.stream_id ASC;
     """
-
     conn = get_connection()
     try:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query)
-            rows = cur.fetchall()
-
-            df = pd.DataFrame(rows)
-            df.index = df.index + 1
+            df = pd.DataFrame(cur.fetchall())
             return df
     finally:
         conn.close()
